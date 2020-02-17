8395a9a7ae1efa3d3b3c0c6b2ae9e490b674f982
Roy <dekota@126.com>
2020-02-17 14:26:41 +0800

    自动化启动集群

--- centos7-hadoop/start-cluster.sh
+++ centos7-hadoop/start-cluster.sh
@@ -6,23 +6,43 @@ if [ "$1"x == "init"x ]; then
    read -p "你确定要重新初始化吗？这将导致以前的初始化数据丢失！（yN）: " answer
 
    if [ "$answer"x == "y"x ]; then
-      echo "开始初始化hadoop集群..."
+      echo "---------------开始初始化hadoop集群---------------"
 
-   else
-      echo "开始启动hadoop集群..."
+      echo "启动journalnode..."
+      jnodes=("hadoop1" "hadoop2" "hadoop3")
+      # shellcheck disable=SC2068
+      for jnode in ${jnodes[@]}; do
+          ssh -q hadoop@$jnode "$HADOOP_HOME/sbin/hadoop-daemon.sh start journalnode"
+          echo "$jnode journalnode started done."
+      done
+
+      echo "格式化hdfs..."
+      ssh -q hadoop@hadoop1 "hadoop namenode -format"
+
+      sleep 3
+
+      echo "启动namenode..."
+      ssh -q hadoop@hadoop1 "$HADOOP_HOME/sbin/hadoop-daemon.sh start namenode"
+
+      sleep 3
+
+      echo "同步namenode元数据..."
+      ssh -q hadoop@hadoop2 "hadoop namenode -bootstrapStandby"
+
+      sleep 3
+
+      echo "初始化ZKFC..."
+      ssh -q hadoop@hadoop1 "hdfs zkfc -formatZK"
+
    fi
-
-else
-   echo "开始启动hadoop集群..."
-
 fi
 
-
-echo "启动hdfs"
+echo "---------------开始启动hadoop集群-----------------"
+echo "启动hdfs..."
 ssh -q hadoop@hadoop1 "$HADOOP_HOME/sbin/start-dfs.sh"
 echo "HDFS started done."
 
-echo "启动Yarn"
+echo "启动Yarn..."
 ssh -q hadoop@hadoop1 "$HADOOP_HOME/sbin/start-yarn.sh"
 echo "YARN started done."
 
@@ -31,18 +51,18 @@ rmArray=("hadoop3" "hadoop4")
 # shellcheck disable=SC2068
 for rnode in ${rmArray[@]}; do
     ssh -q hadoop@$rnode "$HADOOP_HOME/sbin/yarn-daemon.sh start resourcemanager"
-    echo "$rnode resourcemanager start done."
+    echo "$rnode resourcemanager started done."
 done
 
-echo "启动jobhistory"
+echo "启动jobhistory..."
 ssh -q hadoop@hadoop1 "$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver"
 echo "jobhistory started done."
 
 
-echo "启动Hbase"
+echo "启动Hbase..."
 ssh -q hadoop@hadoop4 "$HBASE_HOME/bin/start-hbase.sh"
 echo "Hbase started done."
 
-
+echo "----------------------集群启动成功----------------------"
 
 
