8f43806f29e5920220b46656fc708a2d833a54dd
Roy <dekota@126.com>
2020-02-22 16:20:53 +0800

    update

---
+++ centos7-hadoop/start-hive.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+if [ "$1"x == "init"x ]; then
+  echo "--------------- 在HDFS创建Hive数据存储目录 -----------------"
+
+  ssh -q hadoop@hadoop1 "
+  $HADOOP_HOME/bin/hadoop fs -mkdir /hive/warehouse
+  $HADOOP_HOME/bin/hadoop fs -mkdir /hive/tmp
+  $HADOOP_HOME/bin/hadoop fs -mkdir /hive/logs
+  $HADOOP_HOME/bin/hadoop fs -chmod -R 777 /hive/warehouse
+  $HADOOP_HOME/bin/hadoop fs -chmod -R 777 /hive/tmp
+  $HADOOP_HOME/bin/hadoop fs -chmod -R 777 /hive/logs
+  "
+  echo ""
+
+  sleep 3
+
+  echo "--------------- 初始化Hive元数据库 -----------------"
+  ssh -q hadoop@hadoop1 "$HIVE_HOME/bin/schematool -dbType mysql -initSchema"
+  echo ""
+
+fi
+
+
+echo "--------------- 启动Hive metestore元数据服务 -----------------"
+
+ssh -q hadoop@hadoop1 "$HIVE_HOME/bin/hive --service metastore &"
+ssh -q hadoop@hadoop2 "$HIVE_HOME/bin/hive --service metastore &"
+echo ""
+
+sleep 3
+
+echo "--------------- 启动Hive hiveserver2服务 -----------------"
+ssh -q hadoop@hadoop1 "$HIVE_HOME/bin/hive --service hiveserver2 &"
+ssh -q hadoop@hadoop2 "$HIVE_HOME/bin/hive --service hiveserver2 &"
+echo ""
+
+echo "--------------- 启动Hive服务启动完成 -----------------"
+
