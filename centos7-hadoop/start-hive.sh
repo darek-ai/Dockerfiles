bae92837489e7d60d9f8b5e7743e27d54acc13a0
Roy <dekota@126.com>
2020-02-23 09:16:14 +0800

    update

--- centos7-hadoop/start-hive.sh
+++ centos7-hadoop/start-hive.sh
@@ -3,16 +3,20 @@
 if [ "$1"x == "init"x ]; then
   echo "--------------- 在HDFS创建Hive数据存储目录 -----------------"
 
+  echo "hadoop fs -mkdir /hive"
+  $HADOOP_HOME/bin/hadoop fs -mkdir /hive
+  echo "hadoop fs -mkdir /hive/warehouse"
   $HADOOP_HOME/bin/hadoop fs -mkdir /hive/warehouse
+  echo "hadoop fs -mkdir /hive/tmp"
   $HADOOP_HOME/bin/hadoop fs -mkdir /hive/tmp
+  echo "hadoop fs -mkdir /hive/logs"
   $HADOOP_HOME/bin/hadoop fs -mkdir /hive/logs
+  echo "给目录赋读写权限..."
   $HADOOP_HOME/bin/hadoop fs -chmod -R 777 /hive/warehouse
   $HADOOP_HOME/bin/hadoop fs -chmod -R 777 /hive/tmp
   $HADOOP_HOME/bin/hadoop fs -chmod -R 777 /hive/logs
 
   echo ""
-
-  sleep 3
 
   echo "--------------- 初始化Hive元数据库 -----------------"
   $HIVE_HOME/bin/schematool -dbType mysql -initSchema
@@ -22,14 +26,18 @@ fi
 
 
 echo "--------------- 启动Hive metestore元数据服务 -----------------"
-
+echo "hadoop1节点启动: hive --service metastore &"
 ssh -q hadoop@hadoop1 "$HIVE_HOME/bin/hive --service metastore &"
+
+echo "hadoop2节点启动: hive --service metastore &"
 ssh -q hadoop@hadoop2 "$HIVE_HOME/bin/hive --service metastore &"
 echo ""
 
 
 #echo "--------------- 启动Hive hiveserver2服务 -----------------"
+#echo "hadoop1节点启动: hive --service hiveserver2 &"
 #ssh -q hadoop@hadoop1 "$HIVE_HOME/bin/hive --service hiveserver2 &"
+#echo "hadoop2节点启动: hive --service hiveserver2 &"
 #ssh -q hadoop@hadoop2 "$HIVE_HOME/bin/hive --service hiveserver2 &"
 #echo ""
 
